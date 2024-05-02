41057b38a753d161107eac1f203dcd353e2fc191
R3Kr <thefungigg@gmail.com>
2024-05-02 15:46:35 +0200

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind()],
-  output: "server"
+  output: "server",
+  adapter: vercel()
 });
